39e10ccc2d38a0f0429e138b63322122cf2881e8
Pablo Nunez Navarro <v-panu@microsoft.com>
2019-02-11 15:51:23 +0100

    Restored original delays but with bigger gaps so 3 is always before than 4 & 5.

--- test/cargoQueue.js
+++ test/cargoQueue.js
@@ -11,7 +11,7 @@ describe('cargoQueue', () => {
 
     it('cargoQueue', (done) => {
         var call_order = [],
-            delays = [80, 80, 40];
+            delays = [40, 50, 60];
 
         // worker: --12--34--5-
         // order of completion: 1,2,3,4,5
@@ -46,7 +46,7 @@ describe('cargoQueue', () => {
                 expect(c.length()).to.equal(0);
                 call_order.push('callback ' + 3);
             });
-        }, 20);
+        }, 15);
         setTimeout(() => {
             c.push(4, (err, arg) => {
                 expect(err).to.equal('error');
@@ -61,7 +61,7 @@ describe('cargoQueue', () => {
                 expect(c.length()).to.equal(0);
                 call_order.push('callback ' + 5);
             });
-        }, 80);
+        }, 30);
 
 
         c.drain = function () {
